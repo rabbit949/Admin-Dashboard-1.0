8da84f75f32a4cd45ab8a5ab326580a20bdc74c0
White Rabbit <47401029+rabbit949@users.noreply.github.com>
2022-10-04 01:00:58 +0500

    widgets component complete

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
+import Widgets from '../../components/widgets/Widgets';
 import './home.scss';
 
 const Home = () => {
@@ -9,7 +10,12 @@ const Home = () => {
 			<Sidebar />
 			<div className='home_container'>
 				<Navbar />
-				home container
+				<div className='widgetsContainer'>
+					<Widgets type='users' />
+					<Widgets type='orders' />
+					<Widgets type='earnings' />
+					<Widgets type='balance' />
+				</div>
 			</div>
 		</div>
 	);
