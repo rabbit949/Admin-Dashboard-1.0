2fdd88e047d132e24b40ceb960aeb0ff96d0df6e
White Rabbit <47401029+rabbit949@users.noreply.github.com>
2022-10-06 03:37:16 +0500

    carts section + table

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,54 @@
+import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined';
+import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
+import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
+
+import React from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import './featured.scss';
+
+const Featured = () => {
+	return (
+		<div className='featured'>
+			<div className='top'>
+				<h2 className='title'>Total Revenue</h2>
+				<MoreVertOutlinedIcon fontSize='small' className='icon' />
+			</div>
+			<div className='bottom'>
+				<div className='featuredChart'>
+					<CircularProgressbar value={70} text={'70%'} strokeWidth={5} />
+				</div>
+				<p className='title'>Total sales made today</p>
+				<p className='amount'>$200</p>
+				<p className='desc'>
+					Previous transactions processing. Last payment may not be inlcuded.
+				</p>
+				<div className='summary'>
+					<div className='item'>
+						<div className='itemTitle'>Target</div>
+						<div className='itemResult positive'>
+							<KeyboardArrowUpOutlinedIcon fontSize='small' />
+							<div className='resultAmount'>$14.5k</div>
+						</div>
+					</div>
+					<div className='item'>
+						<div className='itemTitle'>Last Week</div>
+						<div className='itemResult negative'>
+							<KeyboardArrowDownOutlinedIcon fontSize='small' />
+							<div className='resultAmount'>$8.9k</div>
+						</div>
+					</div>
+					<div className='item'>
+						<div className='itemTitle'>Last Month</div>
+						<div className='itemResult positive'>
+							<KeyboardArrowUpOutlinedIcon fontSize='small' />
+							<div className='resultAmount'>$15.5k</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Featured;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import Charts from '../../components/charts/Charts';
+import Featured from '../../components/featured/Featured';
 import Navbar from '../../components/navbar/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
+import Table from '../../components/table/Table';
 import Widgets from '../../components/widgets/Widgets';
 import './home.scss';
 
@@ -15,6 +18,14 @@ const Home = () => {
 					<Widgets type='orders' />
 					<Widgets type='earnings' />
 					<Widgets type='balance' />
+				</div>
+				<div className='charts'>
+					<Featured />
+					<Charts />
+				</div>
+				<div className='listContainer'>
+					<div className='listTitle'>Lastest Trasnactions</div>
+					<Table />
 				</div>
 			</div>
 		</div>
