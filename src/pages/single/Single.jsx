c18160b3a37cb7f2dfa307695857900c3db23436
White Rabbit <47401029+rabbit949@users.noreply.github.com>
2022-10-09 02:44:01 +0500

    created single item widgets

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,8 +1,55 @@
-import React from 'react';
+import EditIcon from '@mui/icons-material/Edit';
+import Navbar from '../../components/navbar/Navbar';
+import Sidebar from '../../components/sidebar/Sidebar';
 import './single.scss';
 
 const Single = () => {
-	return <div>Single</div>;
+	return (
+		<div className='single'>
+			<Sidebar />
+			<div className='singleContainer'>
+				<Navbar />
+				<div className='top'>
+					<div className='left'>
+						<span className='editButton'>
+							<EditIcon className='icon' />
+						</span>
+						<h2 className='title'>Information</h2>
+						<div className='item'>
+							<img
+								src='https://www.belloflostsouls.net/wp-content/uploads/2022/06/JonSnow.jpg'
+								alt='item image'
+								className='itemImage'
+							/>
+							<div className='details'>
+								<h3 className='itemName'>Jon Snow</h3>
+								<div className='detailItem'>
+									<span className='itemKey'>Email: </span>
+									<span className='itemValue'>jonsnow@gmail.com</span>
+								</div>
+								<div className='detailItem'>
+									<span className='itemKey'>Phone: </span>
+									<span className='itemValue'>+92 3333 33 33</span>
+								</div>
+								<div className='detailItem'>
+									<span className='itemKey'>Address: </span>
+									<span className='itemValue'>
+										Castle Black Nightfort (historical)
+									</span>
+								</div>
+								<div className='detailItem'>
+									<span className='itemKey'>Country: </span>
+									<span className='itemValue'>The North</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className='right'></div>
+				</div>
+				<div className='bottom'></div>
+			</div>
+		</div>
+	);
 };
 
 export default Single;
